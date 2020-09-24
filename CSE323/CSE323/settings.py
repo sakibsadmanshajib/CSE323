75ef28162c9105e6f8d63bcc8b5246fa4d805de1
Sakib Sadman Shajib <sakibsadmanshajib@gmail.com>
2020-09-24 10:38:55 +0600

    fixed errors

--- CSE323/CSE323/settings.py
+++ CSE323/CSE323/settings.py
@@ -112,6 +112,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
